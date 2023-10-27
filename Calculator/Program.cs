0604b75cb48e2e039476bb48820b95874b56a8ae
mgentile77 <mgentile1@ivytech.edu>
2023-10-27 19:00:18 -0400

    Completed Program and easy navigation

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,8 +1,4 @@
-﻿
-
-using System.Threading.Channels;
-
-Console.WriteLine("Hello!");
+﻿Console.WriteLine("Hello!");
 Console.WriteLine();
 do {
     Console.WriteLine("What would you like to do");
@@ -12,16 +8,12 @@ do {
     Console.WriteLine("[D]ivide numbers");
     Console.WriteLine("[E]xit");
     var userMathRequested = Console.ReadLine();
-    if (userMathRequested.ToUpper() == "E")
+    var input = userMathRequested.ToUpper();
+    if (input == "E")
     {
         break;
     }
-    else if (userMathRequested.ToUpper() != "A" || userMathRequested.ToUpper() != "S" ||
-        userMathRequested.ToUpper() != "M" || userMathRequested.ToUpper() != "D")
-    {
-        Console.WriteLine("Input is invalid!");
-    }
-    else
+    else if (input is "A" or "S" or "M" or "D")
     {
         Console.WriteLine("Input first number: ");
         var userInput1 = Console.ReadLine();
@@ -29,24 +21,27 @@ do {
         Console.WriteLine("Input second number: ");
         var userInput2 = Console.ReadLine();
         var number2 = int.Parse(userInput2);
-        string outputToUser = OutputToUser(number1, number2, userMathRequested);
+        string outputToUser = OutputToUser(number1, number2, input);
         Console.WriteLine(outputToUser);
     }
+    else
+    {
+        Console.WriteLine("Input is invalid!");
+    }
 } while (true);
-
 string OutputToUser(int num1, int num2, string typeMath)
 {
-    if (typeMath.ToUpper() == "A")
+    if (typeMath == "A")
     {
         var sum = num1 + num2;
         return num1 + "+" + num2 + "=" + sum;
     }
-    else if (typeMath.ToUpper() == "S")
+    else if (typeMath == "S")
     {
         var sum = num1 - num2;
         return num1 + "-" + num2 + "=" + sum;
     }
-    else if (typeMath.ToUpper() == "M")
+    else if (typeMath == "M")
     {
         var sum = num1 * num2;
         return num1 + "X" + num2 + "=" + sum;
@@ -55,7 +50,7 @@ string OutputToUser(int num1, int num2, string typeMath)
     {
         var sum = num1 / num2;
         var remainder = num1 % num2;
-        return num1 + "/" + num2 + "=" + sum + " R " + remainder;
+        return num1 + "/" + num2 + "=" + sum + " R" + remainder;
     }
 }
 
